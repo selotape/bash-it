017cfdd502606379b7e44f62ad4654fe12cd1776
Ron Visbord <RonVisbord@Gmail.com>
2018-01-04 15:57:05 +0200

    improve aliases and elk runner

--- custom/alias.bash
+++ custom/alias.bash
@@ -1,6 +1,4 @@
 alias gpldv='git pull origin dev'
-alias gfrbdv='git fetch origin dev && git rebase origin/dev'
-alias gfmdv='git fetch origin dev && git merge origin/dev'
 
 
 alias updatedb='sudo updatedb'
