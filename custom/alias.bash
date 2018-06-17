cae872dc0c55cea588d50e21d420f796db4bbec4
Ron Visbord <RonVisbord@Gmail.com>
2018-06-17 10:05:43 +0300

    commit all recent scripts

--- custom/alias.bash
+++ custom/alias.bash
@@ -2,5 +2,5 @@ alias gpldv='git pull origin dev'
 
 
 alias updatedb='sudo updatedb'
-alias mci='mvn clean install'
+alias mci='mvn -pl '!tools' clean install'
 alias mciall='mci -Psolr521 && mci -Psolr630 && mci -Pes240'
